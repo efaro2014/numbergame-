f82dbc8408bdd2eccaa855312fa05b68a45f6a69
efaro2014 <51982646+efaro2014@users.noreply.github.com>
2020-10-13 17:07:22 -0700

    Create number_game.py

---
+++ number_game.py
@@ -0,0 +1,49 @@
+import random
+
+randomNumber = random.randint(1,10)
+
+print('guess a number between 1 and 10')
+
+trial = 0
+
+while trial < 5:
+
+try:
+    guess = int(input('you guess a number'))
+    trial += 1
+
+    if guess == randomNumber:
+        print("you got it ")
+
+        if trial == 1:
+            print("you are amazing!")
+            print("you got it in a single shot!")
+        elif trial < 4 and trial > 1:
+            print("you are a good guesser!")
+            print("you got it in {} tries".format(trial))
+        else:
+            print("you got it in {} tries".format(trial))
+
+        print('The number that i was thinking is : ', randomNumber)
+        exit()
+    elif guess not in range(1,10):
+        print(" That's not a valid number, please try again! ")
+
+    elif guess > randomNumber:
+        print("please try lesser number!")
+
+    elif guess < randomNumber:
+        print('please try greater number!')
+
+   
+except ValueError:
+    print(" It's not Valid, Please try again, it has to be an integer!")
+    continue
+
+print('\n')
+
+print('you missed it!')
+
+print("oh sorry you run out of guesses")
+
+print(' The number that i was thinking is : ', randomNumber)
